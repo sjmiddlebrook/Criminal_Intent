9becfe3945ef5f77822a095d0230c025fb5ee387
Jack Middlebrook <jack.middlebrook@gmail.com>
2015-09-30 18:00:13 +0100

    add options menu to toolbar

--- app/src/main/java/com/jackmiddlebrook/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/jackmiddlebrook/criminalintent/CrimePagerActivity.java
@@ -4,10 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.List;
 import java.util.UUID;
@@ -16,7 +16,7 @@ import java.util.UUID;
  * ViewPager for Crimes
  * Created by jack on 9/28/15.
  */
-public class CrimePagerActivity extends FragmentActivity {
+public class CrimePagerActivity extends AppCompatActivity {
 
     private static final String EXTRA_CRIME_ID = "com.jackmiddlebrook.criminalintent.crime_id";
 
