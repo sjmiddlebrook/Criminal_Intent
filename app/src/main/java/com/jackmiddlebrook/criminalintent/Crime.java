923da8d6c70bd58837dc3bc1ca6b4c95ef2caa13
Jack Middlebrook <jack.middlebrook@gmail.com>
2015-10-02 22:01:54 +0100

    add suspect column to database

--- app/src/main/java/com/jackmiddlebrook/criminalintent/Crime.java
+++ app/src/main/java/com/jackmiddlebrook/criminalintent/Crime.java
@@ -13,6 +13,7 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private String mSuspect;
 
     public Crime() {
         this(UUID.randomUUID());
@@ -54,5 +55,13 @@ public class Crime {
 
     public void setSolved(boolean solved) {
         mSolved = solved;
+    }
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public void setSuspect(String suspect) {
+        mSuspect = suspect;
     }
 }
